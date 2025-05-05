b2eaf67a850f6773af21da436be9721e57449327
Xinyu Wu <101110045+pesenteur@users.noreply.github.com>
2025-05-05 11:39:38 +0800

    update

--- parse_args.py
+++ parse_args.py
@@ -2,9 +2,10 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--city',                 default="NY",       help='City name, can be NY or Chi or SF')
-parser.add_argument('--data_path',                    default='./NewYork')
 args = parser.parse_args()
 if args.city == 'NY':
     parser.add_argument('--data_path',                    default='./NewYork')
 else:
     parser.add_argument('--data_path',                    default='./Chicago')
+
+args = parser.parse_args()
